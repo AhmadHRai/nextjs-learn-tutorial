25e859be63afb04e247c4b69947d519bef65a6c3
Ahmad Alrai <ahmadhalraie@gmail.com>
2024-01-09 22:55:00 +0300

    Chapter 16 - Extra

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,11 +2,27 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
-import { Metadata } from 'next';
+import { Metadata, ResolvingMetadata } from 'next';
 
-export const metadata: Metadata = {
-  title: 'Edit Invoice',
-};
+// export const metadata: Metadata = {
+//   title: 'Edit Invoice',
+// };
+
+export async function generateMetadata({ params }: { params: { id: string } }) {
+  const id = params.id;
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+
+  const thisCustomer = customers.find(
+    (customer) => customer.id === invoice.customer_id,
+  );
+
+  return {
+    title: `Edit Invoice - ${thisCustomer?.name}`,
+  };
+}
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
